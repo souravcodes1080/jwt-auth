d5b04c14a1d62f55b6040e8042698b75ea4ea617
SouravCodes1080 <107361153+souravcodes1080@users.noreply.github.com>
2024-05-23 23:24:22 +0530

    otp section ui updated

--- frontend/src/components/Homepage.jsx
+++ frontend/src/components/Homepage.jsx
@@ -8,6 +8,7 @@ import google from "../assets/google.png";
 import github from "../assets/github.png";
 import eyeOpen from "../assets/eyeOpen.jpg";
 import eyeClose from "../assets/eyeClose.jpg";
+import OtpInput from "react-otp-input";
 import axios from "axios";
 import { toast } from "react-toastify";
 function Homepage() {
@@ -18,7 +19,8 @@ function Homepage() {
   const [password, setPassword] = useState("");
   const [hide, setHide] = useState(true);
 
-
+  const [showOtp, setShowOtp] = useState(false);
+  const [otp, setOtp] = useState("");
   useEffect(() => {
     if (cookies["token"]) {
       navigate("/dashboard");
@@ -39,61 +41,125 @@ function Homepage() {
     if (response.data.success) {
       toast.success(response.data.message);
       setCookies("email", email);
-      navigate("/verify");
+      setShowOtp(true);
+      // navigate("/verify");
     } else {
       toast.error(response.data.message);
     }
   };
+
+  const onSubmitOtp = async (e) => {
+    e.preventDefault();
+
+    const response = await axios.post("http://localhost:8080/api/user/verify", {
+      otp: Number(otp),
+      email: cookies.email,
+    });
+    if (response.data.success) {
+      toast.success(response.data.message);
+      navigate("/login");
+    } else {
+      toast.error(response.data.message);
+    }
+  };
+
+  const resendOtp = async () =>{
+    const response = await axios.post("http://localhost:8080/api/user/resend", {
+      email: cookies.email
+    });
+    if (response.data.success) {
+      toast.success(response.data.message);
+  } else {
+    toast.error(response.data.message);
+  }
+  }
   return (
     <>
       <div className="auth">
         <div className="register-page">
-          <div className="register-form">
-            <div className="register-form-wrapper">
-              <img src={logo} alt="" className="logo" />
-              <h2>Welcome to Dev!</h2>
-              <p className="desc">Register with your email and get started.</p>
-              <form onSubmit={onSubmitHandler}>
-                <div className="input-wrapper">
-                  <label>Username*</label>
-                  <input
-                    required
-                    type="text"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                    placeholder="Enter username"
+          {showOtp ? (
+            <div className="register-form">
+              <div className="register-form-wrapper">
+                <img src={logo} alt="" className="logo" />
+                <h2>Verify Otp</h2>
+                <p className="desc">
+                  An otp has been sent to your email address.
+                </p>
+                <form onSubmit={onSubmitOtp}>
+                  <OtpInput
+                    value={otp}
+                    onChange={setOtp}
+                    numInputs={5}
+                    shouldAutoFocus={true}
+                    containerStyle={true}
+                    className={"otp-input"}
+                    inputStyle={true}
+                    renderSeparator={<span>&nbsp;&nbsp;</span>}
+                    renderInput={(props) => <input {...props} className="otp-input" />}
                   />
-                </div>
-                <div className="input-wrapper">
-                  <label>Email*</label>
-                  <input
-                    required
-                    type="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    placeholder="Enter email address"
-                  />
-                </div>
-                <div className="input-wrapper">
-                  <label>Password*</label>
-                  <input
-                  className="password"
-                    required
-                    type={hide ? "password" : "text"}
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    placeholder="Create strong password"
-                  />
-                  <div onClick={()=>setHide(!hide)}>
-                    {
-                    hide? <img className="hide" src={eyeClose} alt="" width={"20px"} />:<img className="hide" src={eyeOpen} alt="" width={"20px"} />
-                  }
+                  <button type="submit">Verify</button>
+                  <p className="resend" onClick={resendOtp}>Resend otp?</p>
+                </form>
+              </div>
+            </div>
+          ) : (
+            <div className="register-form">
+              <div className="register-form-wrapper">
+                <img src={logo} alt="" className="logo" />
+                <h2>Welcome to Dev!</h2>
+                <p className="desc">
+                  Register with your email and get started.
+                </p>
+                <form onSubmit={onSubmitHandler}>
+                  <div className="input-wrapper">
+                    <label>Username*</label>
+                    <input
+                      required
+                      type="text"
+                      value={username}
+                      onChange={(e) => setUsername(e.target.value)}
+                      placeholder="Enter username"
+                    />
                   </div>
-                  
-                    
-                  
-                </div>
-                {/* <div className="input-wrapper terms">
+                  <div className="input-wrapper">
+                    <label>Email*</label>
+                    <input
+                      required
+                      type="email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      placeholder="Enter email address"
+                    />
+                  </div>
+                  <div className="input-wrapper">
+                    <label>Password*</label>
+                    <input
+                      className="password"
+                      required
+                      type={hide ? "password" : "text"}
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      placeholder="Create strong password"
+                    />
+                    <div onClick={() => setHide(!hide)}>
+                      {hide ? (
+                        <img
+                          className="hide"
+                          src={eyeClose}
+                          alt=""
+                          width={"20px"}
+                        />
+                      ) : (
+                        <img
+                          className="hide"
+                          src={eyeOpen}
+                          alt=""
+                          width={"20px"}
+                        />
+                      )}
+                    </div>
+                  </div>
+                  {/* <div className="input-wrapper terms">
                   <div>
 
                   <input
@@ -105,26 +171,31 @@ function Homepage() {
                   </div>
                 </div> */}
 
-                <button type="submit">Register</button>
-                <p>Already have an account? <span onClick={()=>navigate("/login")}>Log In</span></p>
-              </form>
-              <div className="or">
-                <div></div>
-                <p>or</p>
-                <div></div>
-              </div>
+                  <button type="submit">Register</button>
+                  <p>
+                    Already have an account?{" "}
+                    <span onClick={() => navigate("/login")}>Log In</span>
+                  </p>
+                </form>
+                <div className="or">
+                  <div></div>
+                  <p>or</p>
+                  <div></div>
+                </div>
 
-              <button className="google-btn" >
-                <img src={google} alt="" width={"20px"} />
-                Sign in with Google
-              </button>
-              <button className="google-btn">
-                {" "}
-                <img src={github} alt="" width={"20px"} />
-                Sign in with Github
-              </button>
+                <button className="google-btn">
+                  <img src={google} alt="" width={"20px"} />
+                  Sign in with Google
+                </button>
+                <button className="google-btn">
+                  {" "}
+                  <img src={github} alt="" width={"20px"} />
+                  Sign in with Github
+                </button>
+              </div>
             </div>
-          </div>
+          )}
+
           <div className="register-image">
             <img src={hero} alt="hero pic" />
           </div>
