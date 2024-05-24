aee26de080b29231643c3c02bde78c81fc8ec4f8
SouravCodes1080 <107361153+souravcodes1080@users.noreply.github.com>
2024-05-24 15:47:21 +0530

    code cleanup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,17 +4,15 @@ import "react-toastify/dist/ReactToastify.css";
 import { Routes, Route } from "react-router-dom";
 import Homepage from "./components/Homepage";
 import Login from "./components/Login";
-import Register from "./components/Register";
 import Dashboard from "./components/Dashboard";
-import Verify from "./components/Verify";
+
 function App() {
   return (
     <div className="app-container">
       <Routes>
         <Route path="/" exact element={<Homepage />} />
         <Route path="/login" exact element={<Login />} />
-        <Route path="/register" exact element={<Register />} />
-        <Route path="/verify" exact element={<Verify />} />
+        
         <Route path="/dashboard" exact element={<Dashboard />} />
       </Routes>
       <ToastContainer
